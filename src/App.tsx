946447915f82e57fb627856960107628fb70dc45
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-18 18:04:53 +0200

    lab3

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import { FC } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./componenst/Routes/Home/home";
 import { Posts } from "./componenst/Routes/Posts/posts"
-
+import { GlobalStyle } from "./componenst/style/global";
+import { Layout } from "./componenst/Common/layout";
 
 export const App: FC = () => {
   return (
+    <>
+    <GlobalStyle/>
     <BrowserRouter>
       <div>
         <Link to="posts">Posts</Link>
         <Link to="/">Home</Link>
       </div>
+      <Layout>
       <Routes>
         <Route path="posts" element={<Posts />} />
         <Route index element={<Home />} />
       </Routes>
+      </Layout>
     </BrowserRouter>
+    </>
   );
 }
 
